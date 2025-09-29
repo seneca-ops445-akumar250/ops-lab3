306da59de12455ce381261f7db65652a51fccb0f
Amit Kumar <akumar250@myseneca.ca>
2025-09-29 17:43:10 -0400

    Completed Lab 3 with all scripts and checkscript outputs

--- lab3e.py
+++ lab3e.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
-
 # Author ID: akumar250
 
+# Global list
 my_list = [100, 200, 300, 'six hundred']
 
 def give_list():
+    # Does not accept any arguments
+    # Returns all of items in the global object my_list unchanged
     return my_list
 
 def give_first_item():
-    return my_list[0]
+    # Does not accept any arguments
+    # Returns the first item in the global object my_list as a string
+    return str(my_list[0])
 
 def give_first_and_last_item():
+    # Does not accept any arguments
+    # Returns a list that includes the first and last items in the global object my_list
     return [my_list[0], my_list[-1]]
 
 def give_second_and_third_item():
-    return my_list[1:3]
+    # Does not accept any arguments
+    # Returns a list that includes the second and third items in the global object my_list
+    return [my_list[1], my_list[2]]
 
-if __name__ == '__main__':
+if __name__ == '__main__':   # Main block
     print(give_list())
     print(give_first_item())
     print(give_first_and_last_item())
